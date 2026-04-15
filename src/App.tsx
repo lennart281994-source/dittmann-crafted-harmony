95ab3ec54f1d3954077ad351962c0c770e443cf9
lennart281994-source <lennart281994@gmail.com>
2026-04-15 19:12:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/dittmann-crafted-harmony">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
